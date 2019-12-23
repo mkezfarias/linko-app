32e7267696ff010d04360a6bb3999f8a1612316e
camfilho <camfilho@ufrn.edu.br>
2019-12-23 18:32:56 -0300

    add relatioship between users and links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
     validates :url, :priority,  presence: true
     validates :url, length: { minimum: 5 }
+
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_211140) do
+ActiveRecord::Schema.define(version: 2019_12_23_213035) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_12_23_211140) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
